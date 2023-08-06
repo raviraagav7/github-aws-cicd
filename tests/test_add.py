2f735eef998561a40251cef08b3ac6b6414fe7bd
raviraagav7 <raviraagav7@gmail.com>
2023-08-05 22:38:38 -0700

    Fixed the test pipeline

--- tests/test_add.py
+++ tests/test_add.py
@@ -9,7 +9,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_add(self):
         result = add(2, 3)
-        self.assertEqual(result, 6)
+        self.assertEqual(result, 5)
 
 if __name__ == '__main__':
     unittest.main()
